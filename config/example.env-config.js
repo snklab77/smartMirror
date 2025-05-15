0810617a42020acc2e0bc2c3c6e2b634b86b55a3
snklab <snklab_info@yahoo.co.jp>
2025-05-15 23:16:17 +0900

    sample config追記

--- config/example.env-config.js
+++ config/example.env-config.js
@@ -8,5 +8,15 @@ const ENV = {
   * weatherapiのAPIキーを取得するには、上記のURLにアクセスしてアカウントを作成してください。
   * APIキーは、アカウント作成後にダッシュボードから取得できます。
   */
-  WEATHERAPI_KEY: "YOUR-API-KEY-HERE"
+  WEATHERAPI_KEY: "YOUR-API-KEY-HERE",
+
+  /*
+  * 位置情報設定
+  * 緯度経度は小数点以下6桁程度まで指定可能です
+  */
+  LOCATION: {
+    lat: 35.681143654455774,
+    lon: 139.76749021382773,
+    name: "品川区"
+  }
 };
